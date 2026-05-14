d8b8ef088d04db3fc9ceea27f303b87f411f9dae
Voidbert <humbertogilgomes@protonmail.com>
2026-05-14 19:34:06 +0100

    Add job for threading and NUMA analysis

---
+++ TP2/jobs/threading-test.sh
@@ -0,0 +1,154 @@
+#!/bin/sh
+
+# ABOUT --------------------------------------------------------------------------------------------
+#
+# Deucalion job for testing if model performance varies with different numbers of threads and
+# NUMA configuration.
+#
+# Usage: $ ./threading-test.sh
+#
+# LICENSE ------------------------------------------------------------------------------------------
+#
+# Copyright (C) 2026 Humberto Gomes, José Lopes, José Soares
+#
+# This file is part of FastInference.
+#
+# FastInference is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# FastInference is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with FastInference. If
+# not, see <https://www.gnu.org/licenses/>.
+#
+# SLURM PROPERTIES ---------------------------------------------------------------------------------
+#
+# General properties
+#SBATCH --job-name=threading-test
+#SBATCH --time=01:00:00
+#SBATCH --output=output-%j.out
+#
+# Run on ARM systems
+#SBATCH --partition=normal-arm
+#SBATCH --account=f202500010hpcvlabuminhoa
+#
+# Manage number of tasks
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#
+# CONFIGURATION ------------------------------------------------------------------------------------
+
+# Directory where to output the threading results to
+OUTPUT_DIRECTORY='../results/threading'
+
+# llama.cpp build to test
+LLAMA_CPP_BUILD_CONFIGURATION='build-clang-blis'
+
+# Model quantizations to test
+QUANTIZATION='Q4_K_M'
+
+# Numbers of threads to test
+THREADS='12 24 36 44 48'
+
+# Seed to be used for all prompts
+SEED=0
+
+# Number of runs per prompt and model (reduced to six to save time)
+RUNS=6
+
+# Maximum number of tokens per reply
+MAX_TOKENS=4096
+
+# JOB ----------------------------------------------------------------------------------------------
+
+# Parse command-line arguments
+if [ "$#" = 0 ]; then
+    # Not a Slurm job. Relaunch the script as a job for testing multiple models in parallel.
+    mkdir -p "$OUTPUT_DIRECTORY"
+    find '../models' -type f -name "*$QUANTIZATION*" | while IFS= read -r model_file; do
+        for threads in $THREADS; do
+            # Do not test --numa=isolate. It is too slow.
+            for numa in distribute; do
+                sbatch "$0" "$model_file" "$threads" "$numa"
+            done
+        done
+    done
+else
+    # Load necessary modules
+    module purge
+    module load GCC/13.3.0
+    module load LLVM/19.1.7-GCCcore-13.3.0
+    module load CMake/3.31.3-GCCcore-13.3.0
+    module load BLIS/1.0-GCC-13.3.0
+
+    # Script is being run as a Slurm job for testing a single model
+    model_file="$1"
+    threads="$2"
+    numa="$3"
+    model_name="$(basename "$model_file" | sed -E 's/\-Q[0-9_KM]+\.gguf$//')"
+    quantization="$(basename "$model_file" | grep -Eo -- 'Q[0-9_KM]+')"
+    output_file="$OUTPUT_DIRECTORY/$model_name-$quantization-t$threads-$numa.csv"
+
+    # Write output CSV file header
+    printf 'COMPILER,MODEL,QUANTIZATION,SEEDING,PROMPT,THREADS,NUMA,RUN,' >  "$output_file"
+    printf 'PROMPT_N,PREDICTED_N,CACHE_N,TTFT,TPOT,MEM\n'                 >> "$output_file"
+
+    # Ask the model for multiple responses for a prompt of each category
+    find '../prompts' -type f -name '0' | sort -r | while IFS= read -r prompt_file; do
+        escaped_prompt="$(sed 's/"/\\"/g' < "$prompt_file")"
+        prompt_type="$(basename "$(dirname "$prompt_file")")"
+
+        # Run llama.cpp's server in the background. llama.cpp is restarted for every prompt to avoid
+        # KV-cache reuse between prompts.
+        BLIS_NUM_THREADS="$threads"                                        \
+            "../llama.cpp/$LLAMA_CPP_BUILD_CONFIGURATION/bin/llama-server" \
+            -t "$threads" --numa "$numa" --no-mmap -m "$model_file" 2>/dev/null &
+
+        # Wait for the model to load
+        while [ "$(curl -s 'http://localhost:8080/health' | jq -r '.status')" != 'ok' ]; do
+            sleep 1
+        done
+
+        # Provide the same prompt multiple times to the model
+        for run in $(seq "$RUNS"); do
+            # Progress message
+            printf '%s -> Processing %s: %s (run %s)\n' \
+                "$(date +%H:%M:%S)"                     \
+                "$model_name-$quantization"             \
+                "$prompt_type"                          \
+                "$run"
+
+            # Write run information to output file
+            printf '%s,%s,%s,%s,%s,%s,%s,%s,'            \
+                "$LLAMA_CPP_BUILD_CONFIGURATION"         \
+                "$model_name" "$quantization" 'static'   \
+                "$prompt_type" "$threads" "$numa" "$run" >> "$output_file"
+
+            # Obtain reponse from the model and output timing metrics
+            curl -s 'http://localhost:8080/v1/chat/completions' --data '{
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "'"$escaped_prompt"'"
+                    }
+                ],
+                "seed": '"$SEED"',
+                "max_tokens": '"$MAX_TOKENS"'
+            }' | jq -r '
+                .timings |
+                    "\(.prompt_n),\(.predicted_n),\(.cache_n)," +
+                    "\(.prompt_ms + .predicted_per_token_ms),\(.predicted_per_token_ms),"
+            '  | tr -d '\n' >> "$output_file"
+
+            # Get system memory usage in bytes
+            free -b | grep '^Mem:' | awk '{ printf "%s\n", $3 }' >> "$output_file"
+        done
+
+        # Stop the llama.cpp server
+        pkill llama-server
+    done
+fi
